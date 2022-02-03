ca06205fdfe846735080b53c762609a24f9139e8
Vlatko Stojkoski <stojvlatko@gmail.com>
2022-02-03 23:30:42 +0100

    first commit

--- public/script.js
+++ public/script.js
@@ -1,56 +1,125 @@
 const socket = io('/')
+
 const videoGrid = document.getElementById('video-grid')
-const myPeer = new Peer(undefined, {
-  host: '/',
-  port: '3001'
-})
+
+const myPeer = new Peer()
+
 const myVideo = document.createElement('video')
 myVideo.muted = true
+
 const peers = {}
+
 navigator.mediaDevices.getUserMedia({
-  video: true,
-  audio: true
+	video: true,
+	audio: true
 }).then(stream => {
-  addVideoStream(myVideo, stream)
+	addVideoStream(myVideo, stream)
 
-  myPeer.on('call', call => {
-    call.answer(stream)
-    const video = document.createElement('video')
-    call.on('stream', userVideoStream => {
-      addVideoStream(video, userVideoStream)
-    })
-  })
+	myPeer.on('call', call => {
+		call.answer(stream)
 
-  socket.on('user-connected', userId => {
-    connectToNewUser(userId, stream)
-  })
+		const video = document.createElement('video')
+
+		call.on('stream', userVideoStream => {
+			addVideoStream(video, userVideoStream)
+		})
+	})
+
+	socket.on('user-connected', userId => {
+		connectToNewUser(userId, stream)
+	})
 })
 
 socket.on('user-disconnected', userId => {
-  if (peers[userId]) peers[userId].close()
+	if (peers[userId]) peers[userId].close()
 })
 
 myPeer.on('open', id => {
-  socket.emit('join-room', ROOM_ID, id)
+	socket.emit('join-room', ROOM_ID, id)
 })
 
 function connectToNewUser(userId, stream) {
-  const call = myPeer.call(userId, stream)
-  const video = document.createElement('video')
-  call.on('stream', userVideoStream => {
-    addVideoStream(video, userVideoStream)
-  })
-  call.on('close', () => {
-    video.remove()
-  })
+	const call = myPeer.call(userId, stream)
+	const video = document.createElement('video')
 
-  peers[userId] = call
+	call.on('stream', userVideoStream => {
+		addVideoStream(video, userVideoStream)
+	})
+
+	call.on('close', () => {
+		video.remove()
+	})
+
+	peers[userId] = call
 }
 
 function addVideoStream(video, stream) {
-  video.srcObject = stream
-  video.addEventListener('loadedmetadata', () => {
-    video.play()
-  })
-  videoGrid.append(video)
+	video.srcObject = stream
+	video.addEventListener('loadedmetadata', () => {
+		video.play()
+	})
+	videoGrid.append(video)
 }
+
+function toggleFullscreen(elem) {
+	if (!document.fullscreenElement) {
+		document.documentElement.requestFullscreen()
+	} else {
+		if (document.exitFullscreen) {
+			document.exitFullscreen()
+		}
+	}
+}
+
+function toggleExpandClass() {
+	if (document.querySelector('#fullscreen-btn > i').classList.contains('fa-expand')) {
+		document.querySelector('#fullscreen-btn > i').classList.remove('fa-expand')
+		return document.querySelector('#fullscreen-btn > i').classList.add('fa-compress')
+	}
+
+	document.querySelector('#fullscreen-btn > i').classList.remove('fa-compress')
+	document.querySelector('#fullscreen-btn > i').classList.add('fa-expand')
+}
+
+document.querySelector('#fullscreen-btn').addEventListener('click', (e) => {
+	toggleFullscreen(document.documentElement)
+	toggleExpandClass()
+})
+
+function toggleMicrophone() {
+	if (!myVideo.srcObject.getAudioTracks()[0].enabled) {
+		myVideo.srcObject.getAudioTracks()[0].enabled = true
+		document.querySelector('#microphone-btn > i').classList.remove('fa-microphone-slash')
+		return document.querySelector('#microphone-btn > i').classList.add('fa-microphone')
+	}
+
+	myVideo.srcObject.getAudioTracks()[0].enabled = false
+	document.querySelector('#microphone-btn > i').classList.remove('fa-microphone')
+	document.querySelector('#microphone-btn > i').classList.add('fa-microphone-slash')
+}
+
+document.querySelector('#microphone-btn').addEventListener('click', (e) => {
+	toggleMicrophone()
+})
+
+function toggleCamera() {
+	if (myVideo.srcObject.getVideoTracks()[0].enabled) {
+		myVideo.srcObject.getVideoTracks()[0].enabled = false
+		document.querySelector('#video-btn > i').classList.remove('fa-video')
+		return document.querySelector('#video-btn > i').classList.add('fa-video-slash')
+	}
+
+	myVideo.srcObject.getVideoTracks()[0].enabled = true
+	document.querySelector('#video-btn > i').classList.remove('fa-video-slash')
+	document.querySelector('#video-btn > i').classList.add('fa-video')
+}
+
+document.querySelector('#video-btn').addEventListener('click', (e) => {
+	toggleCamera()
+})
+
+document.querySelector('#autoRowsSize').addEventListener('input', (e) => {
+	console.log(e.target.value)
+	document.querySelector('#video-grid').style.gridTemplateColumns = `repeat(auto-fill, ${e.target.value / 4 * 5}rem)`;
+	document.querySelector('#video-grid').style.gridAutoRows = `${e.target.value}rem`;
+})
--- server.js
+++ server.js
@@ -2,28 +2,27 @@ const express = require('express')
 const app = express()
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
-const { v4: uuidV4 } = require('uuid')
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  res.redirect(`/${uuidV4()}`)
-})
-
-app.get('/:room', (req, res) => {
-  res.render('room', { roomId: req.params.room })
+	res.render('room', { roomId: '1' })
 })
 
 io.on('connection', socket => {
-  socket.on('join-room', (roomId, userId) => {
-    socket.join(roomId)
-    socket.to(roomId).broadcast.emit('user-connected', userId)
+	console.log('New user connected')
 
-    socket.on('disconnect', () => {
-      socket.to(roomId).broadcast.emit('user-disconnected', userId)
-    })
-  })
+	socket.on('join-room', (roomId, userId) => {
+		console.log({ roomId, userId })
+
+		socket.join(roomId)
+		socket.to(roomId).broadcast.emit('user-connected', userId)
+
+		socket.on('disconnect', () => {
+			socket.to(roomId).broadcast.emit('user-disconnected', userId)
+		})
+	})
 })
 
 server.listen(3000)
